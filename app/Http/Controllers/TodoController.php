21969f01250a5fac828d5d22388e0722e654b40c
Galambos Tamas <g.tamas@somaweb.eu>
2023-11-12 10:59:05 +0100

    create Detail view layout and controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TODO;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    private $todo_id = 0;
+
+    public function detail(Request $request){
+
+        if(!empty($request) && !empty($request->get("id"))){
+            $this->todo_id = intval($request->get("id"));
+
+            $todo = TODO::GetTodoByID($this->todo_id);
+
+            return View("layouts.detail" , ["todo" => $todo]);
+        }else{
+            return redirect()->to('/');
+        }
+    }
+}
